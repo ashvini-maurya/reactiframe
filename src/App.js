afbefffdf0262e9c4b8eef4912fbaa52ffc3663f
Anirudh Sylendranath <anirudh@esthenos.com>
2020-09-16 16:21:25 +0530

    Use textboxes to render HTML template with JSON

--- src/App.js
+++ src/App.js
@@ -1,34 +1,41 @@
 import React, { useState } from "react";
 import "./App.css";
 import IFrame from "./iframe";
+import * as nunjucks from "nunjucks";
 
 const App = () => {
-  const [textarea, setTextarea] = useState("");
+  const [htmlTemplate, setHtmlTemplate] = useState("");
+  const [jsonContext, setJsonContext] = useState("");
+  const [templateString, setTemplateString] = useState("");
+
+  function renderTemplate() {
+    const jsonifiedContext = JSON.parse(jsonContext);
+    setTemplateString(nunjucks.renderString(htmlTemplate, jsonifiedContext));
+  }
 
   return (
     <div className="app">
       <div className="textarea">
-        <form>
-          <textarea
-            name="textarea"
-            cols="30"
-            rows="3"
-            placeholder="textarea"
-            value={textarea}
-            onChange={(e) => setTextarea(e.target.value)}
-          />
-          <textarea
-            name="textarea2"
-            cols="30"
-            rows="3"
-            placeholder="New textarea"
-          />
-        </form>
+        <textarea
+          name="textarea"
+          cols="30"
+          rows="3"
+          placeholder="HTML template"
+          value={htmlTemplate}
+          onChange={(e) => setHtmlTemplate(e.target.value)}
+        />
+        <textarea
+          name="textarea2"
+          cols="30"
+          rows="3"
+          placeholder="JSON Context"
+          value={jsonContext}
+          onChange={(e) => setJsonContext(e.target.value)}
+        />
+        <button onClick={renderTemplate}>Render</button>
       </div>
 
-      <IFrame>
-        <h1>{textarea}</h1>
-      </IFrame>
+      <IFrame>{templateString}</IFrame>
     </div>
   );
 };
